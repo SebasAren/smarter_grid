6ee4c6aee67c4c68b55046fa5441235b37468e20
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2017-11-04 22:57:40 +0100

    can read houses

--- data_structure.py
+++ data_structure.py
@@ -1,17 +1,43 @@
 # data_structure.py
+import csv
 
 class Grid(object):
-    
+
     # potential matrix for a grid
     def __init__(self, N, M):
         self.matrix = [[0 for i in range(M)] for j in range(N)]
 
 class Battery(Grid):
+    
+    # battery has capacity, position and list of connected houses
     def __init__(self, capacity, position):
         self.capacity = capacity
         self.position = position
         self.houses = []
 
+    # house should be a coordinate tuple
     def connect_house(self, house):
         self.houses.append(house)
-        house.battery.append()
+        self.capacity -= house.capacity
+
+class House(object):
+
+    def __init__(self, x, y, power):
+        self.power = power
+        self.position = (x, y)
+
+def read_houses(f):
+    reader = csv.reader(f)
+    houses = []
+    for row in reader:
+        house = House(row[0], row[1], row[2])
+        houses.append(house)
+    return houses
+
+if __name__ == '__main__':
+    
+    # create a list of houses
+    with open('data/wijk1_huizen.csv') as f:
+        houses = read_houses(f)
+
+    #print(houses)
