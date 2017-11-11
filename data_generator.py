cf8f7cd725368a45a00a73b8150aa60b83769cef
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2017-11-11 21:22:16 +0100

    added error checking

--- data_generator.py
+++ data_generator.py
@@ -21,20 +21,27 @@ def generate_grid(num_h, num_b, x, y):
             battery_power += matrix[y_try][x_try]
             counter_h += 1
 
-    battery_power = math.ceil((battery_power / num_b) * ERROR_MARGIN)  
+    battery_power = math.ceil((battery_power / num_b) * ERROR_MARGIN)
     while counter_b < num_b:
         x_try = random.randrange(x)
         y_try = random.randrange(y)
         if matrix[y_try][x_try] == 0:
             matrix[y_try][x_try] -= battery_power
             counter_b += 1
-            
+
     return matrix
 
 if __name__ == '__main__':
 
-    matrix = generate_grid(int(sys.argv[1]), int(sys.argv[2]),\
-    int(sys.argv[3]), int(sys.argv[4]))
+    try:
+        if (int(sys.argv[1]) + int(sys.argv[2]) < int(sys.argv[3]) * int(sys.argv[4])):
+            matrix = generate_grid(int(sys.argv[1]), int(sys.argv[2]),\
+            int(sys.argv[3]), int(sys.argv[4]))
+        else:
+            sys.exit('grid will be too small')
+    except IndexError:
+        sys.exit('usage: python3 data_generator.py num_houses num_batteries x y')
+        
     print(matrix)
     sum_total = 0
     for row in matrix:
