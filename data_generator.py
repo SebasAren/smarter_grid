db59b42d3642b6fbaf850d51224389100e6edb07
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2017-11-12 13:02:32 +0100

    clean up

--- data_generator.py
+++ data_generator.py
@@ -1,30 +1,46 @@
-import csv
+# data_generator.py
+#
+# Will generate randomized input data for the smart grid problem
+
 import random
 import sys
 import math
 
-BASE_PATH = 'data/self_created/'
 
-MAXPOWER = 10
-ERROR_MARGIN = 1.1
+# CONSTANTS
+MAXPOWER = 1000
+MINPOWER = 10
+ERROR_MARGIN = 1.01
 
+# grid generator
 def generate_grid(num_h, num_b, x, y):
+    
+    # create an empty grid
     matrix = [[0 for i in range(x)] for j in range(y)]
     counter_h = 0
     counter_b = 0
     battery_power = 0
+
+    # first add houses
     while counter_h < num_h:
         x_try = random.randrange(x)
         y_try = random.randrange(y)
+
+        # check if random spot is still empty
         if matrix[y_try][x_try] == 0:
-            matrix[y_try][x_try] += random.randint(1, MAXPOWER)
+
+            # get random power
+            matrix[y_try][x_try] += random.randint(MINPOWER, MAXPOWER)
             battery_power += matrix[y_try][x_try]
             counter_h += 1
 
+    # determine amount of battery power needed and fill up batteries 
     battery_power = math.ceil((battery_power / num_b) * ERROR_MARGIN)
     while counter_b < num_b:
         x_try = random.randrange(x)
         y_try = random.randrange(y)
+
+        # place battery
         if matrix[y_try][x_try] == 0:
             matrix[y_try][x_try] -= battery_power
             counter_b += 1
@@ -33,8 +49,9 @@ def generate_grid(num_h, num_b, x, y):
 
 if __name__ == '__main__':
 
+    # error messages
     try:
-        if (int(sys.argv[1]) + int(sys.argv[2]) < int(sys.argv[3]) * int(sys.argv[4])):
+        if (int(sys.argv[1]) + int(sys.argv[2]) <= int(sys.argv[3]) * int(sys.argv[4])):
             matrix = generate_grid(int(sys.argv[1]), int(sys.argv[2]),\
             int(sys.argv[3]), int(sys.argv[4]))
         else:
@@ -42,6 +59,7 @@ if __name__ == '__main__':
     except IndexError:
         sys.exit('usage: python3 data_generator.py num_houses num_batteries x y')
         
+    # output
     print(matrix)
     sum_total = 0
     for row in matrix:
