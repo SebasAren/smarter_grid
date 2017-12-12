8d00b5e68f196ce522e8c5fcec72364fc2135707
Jelle den Haan <jelle_den_haan@hotmail.nl>
2017-12-12 13:12:34 +0100

    update 2 kabel vis

--- src/visualizations/cable_vis.py
+++ src/visualizations/cable_vis.py
@@ -11,13 +11,13 @@ import pylab as pl
 from matplotlib import collections as mc
 
 
-lines = [[(0, 1), (1, 1)], [(2, 3), (3, 3)], [(1, 2), (1, 3)]]
-c = np.array([(1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1)])
+def draw_cable(lines):
 
-lc = mc.LineCollection(lines, colors=c, linewidths=2)
-fig, ax = pl.subplots()
-ax.add_collection(lc)
-ax.autoscale()
-ax.margins(0.1)
 
-plt.show()
+	lc = mc.LineCollection(lines, linewidths=2)
+	fig, ax = pl.subplots()
+	ax.add_collection(lc)
+	ax.autoscale()
+	ax.margins(1.0)
+
+	plt.show()
