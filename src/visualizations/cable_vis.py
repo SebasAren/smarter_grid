999e8afff9b7e1c1c0e94fb5d6feff9834b13f80
Jelle den Haan <jelle_den_haan@hotmail.nl>
2017-12-12 13:05:32 +0100

    update kabel vis

---
+++ src/visualizations/cable_vis.py
@@ -0,0 +1,23 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '.')))
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..')))
+from data_structure import Battery, House
+import data_generator 
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import pylab as pl 
+from matplotlib import collections as mc
+
+
+lines = [[(0, 1), (1, 1)], [(2, 3), (3, 3)], [(1, 2), (1, 3)]]
+c = np.array([(1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1)])
+
+lc = mc.LineCollection(lines, colors=c, linewidths=2)
+fig, ax = pl.subplots()
+ax.add_collection(lc)
+ax.autoscale()
+ax.margins(0.1)
+
+plt.show()
