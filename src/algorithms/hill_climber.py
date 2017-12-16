1616fe06a2ca3c2220a4eaac2d88fbdceb97a5a1
TPLxxA <caspervanvelzen@hotmail.com>
2017-12-16 16:49:25 +0100

    added comments on most methods

--- src/algorithms/hill_climber.py
+++ src/algorithms/hill_climber.py
@@ -54,6 +54,10 @@ class HillClimber(object):
         self.initial_fit()
 
     def initial_fit(self):
+        """
+        Fits all houses in bins
+        """
+        # try random fit untill valid
         while True:
             try:
                 self.bins = self.random_fit()
@@ -61,6 +65,7 @@ class HillClimber(object):
             except FitError:
                 pass
 
+        # calculate costs
         for i, el in enumerate(self.bins):
             self.cost_values[i] = self.mean_distance_check(el, i)
         
@@ -75,12 +80,18 @@ class HillClimber(object):
 
     # check contstraint
     def constraint_check(self, bucket, b_type=0):
+        """
+        Checks if capcity of bins hasn't been exceeded
+        """
         if self.size_of_bin(bucket) > self.bin_size[b_type]:
             return False
         else:
             return True
 
     def mean_distance_check(self, bucket, battery_id):
+        """
+        Checks mean distance between other objects in bucket
+        """
         cost = 0
         for i, house in enumerate(bucket):
             total_house = self.distance_batteries[battery_id][house.id]
@@ -90,12 +101,16 @@ class HillClimber(object):
         
         return cost
     
-    def improvement_check(self, bin_1, bin_2):    
+    def improvement_check(self, bin_1, bin_2):
+        """
+        Checks for lower costs
+        """
          
-        # check if distance decreases
+        # check mean distance for two bins
         check_1 = self.mean_distance_check(self.bins[bin_1], bin_1)
         check_2 = self.mean_distance_check(self.bins[bin_2], bin_2)
         
+        # check if mean distance is shorter than before
         if (check_1 + check_2) < (self.cost_values[bin_1] + self.cost_values[bin_2]):
             self.cost_values[bin_1] = check_1
             self.cost_values[bin_2] = check_2
@@ -104,44 +119,60 @@ class HillClimber(object):
             return False
 
     def pick_swap(self):
-        # pick random houses to swap 
+        """
+        Picks random houses to swap (doesn't swap them)
+        """
+        # pick two random (different) bins to pick houses from
         bin_1 = random.randrange(len(self.bins))
         bin_2 = bin_1
         while bin_2 == bin_1:
             bin_2 = random.randrange(len(self.bins))
 
+        # pick random house from each bin
         house_1 = random.randrange(len(self.bins[bin_1]))
         house_2 = random.randrange(len(self.bins[bin_2]))        
 
         return house_1, bin_1, house_2, bin_2    
 
     def try_swap(self, house_1, bin_1, house_2, bin_2):
+        """
+        Swaps two houses if conditions are satisfied
+        """
+        #  swap two houses
         self.swap_houses(house_1, bin_1, house_2, bin_2)
 
-        # dit stuk bij constraints check 
-        # check constrainhots, else swap them back and break function
+        # check if capacity has been exceeded in either bin
         if self.constraint_check(self.bins[bin_1]) == True and self.constraint_check(self.bins[bin_2]) == True:
 
             # checks for improvement
             if self.improvement_check(bin_1, bin_2):
                 return True
 
+            #  if no improvement, swap back houses
             else:
                 self.swap_houses(house_1, bin_1, house_2, bin_2)
                 return False
-
+        # if capacity has been exceeded, swap back houses
         else:
             self.swap_houses(house_1, bin_1, house_2, bin_2)
             return False          
 
     def swap_houses(self, house_1, bin_1, house_2, bin_2):
+        """
+        Swaps two houses
+        """
         self.bins[bin_1][house_1], self.bins[bin_2][house_2] = self.bins[bin_2][house_2], self.bins[bin_1][house_1]
 
     # function to start the optimization
     def first_fit(self):
+        """
+        Greedy method to put houses in bins
+        """
 
         # make a list of bins
         bins = [[] for i in range(len(self.batteries))]
+        # for every house, put in emptiest bin
+        # starts at house with highest output
         try:
             for house in self.houses:
                 lowest = self.bin_size[0]
@@ -151,6 +182,7 @@ class HillClimber(object):
                         lowest = current
                         bin_place = i
                 bins[bin_place].append(house)
+                #  check if bin capacity hasn't been exceeded
                 self.constraint_check(bins[bin_place])
         except ConstraintError:
             exit('First fit failed!')
@@ -159,8 +191,10 @@ class HillClimber(object):
 
         return bins
 
-    # function to randomly fit the bins
     def random_fit(self):
+        """
+        Function to randomly fit houses in emptiest bin
+        """
 
         temp_houses = copy.copy(self.houses)
 
@@ -169,17 +203,20 @@ class HillClimber(object):
         smallest_bin = self.bin_size[0]
 
         for n in range(len(self.houses)):
-            # print(len(temp_houses))
+            # pick random house
             house_index = random.randrange(len(temp_houses))
+            # check for emptiest bin
             lowest = self.bin_size[0]
             for i, el in enumerate(bins):
                 current = self.size_of_bin(el)
                 if current < lowest:
                     lowest = current
                     bin_place = i
+            # place house in emptiest bin
             bins[bin_place].append(temp_houses[house_index])
             temp_houses.pop(house_index)
 
+        # check if bin capacity hasn't been exceeded
         for bucket in range(len(bins)):
             if not self.constraint_check(bins[bucket]):
                 raise FitError
@@ -194,10 +231,8 @@ class HillClimber(object):
         max_tries = TRIES
         tries = 0 
         while tries < max_tries:
-            # print(tries)
             if tries == max_tries - 1:
                 permutation = self.test_bins()
-                # print(permutation)
                 if len([i for i, j in zip(permutation, [0, 1, 2, 3, 4]) if i == j]) == 5:
                     tries += 1
                 else:
@@ -220,7 +255,7 @@ class HillClimber(object):
 
     def test_bins(self):
         """
-        Used to return the permutation of batteries in which the 
+        Used to return the permutation of batteries in which the ???
         """
 
         best_swap = copy.copy(self.cost_values)
@@ -236,9 +271,11 @@ class HillClimber(object):
 
         return best_permutation
 
-
-    # save the solution to a csv
     def write_solution(self, best_solution, best_value):
+        """
+        Writes solution to csv
+        """
+
         with open('../../data/solutions/wijk1/solution_{}.csv'.format(best_value), 'a') as outfile:
             writer = csv.writer(outfile)
             for i, el in enumerate(best_solution):
